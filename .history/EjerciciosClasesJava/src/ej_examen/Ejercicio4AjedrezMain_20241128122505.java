08d52a92203ba23315c9154a59b19b1912c3bf1b
Gab <ausejolamadridg@gmail.com>
2024-11-28 13:25:31 +0100

    Added examen

---
+++ .history/EjerciciosClasesJava/src/ej_examen/Ejercicio4AjedrezMain_20241128122505.java
@@ -0,0 +1,10 @@
+package ej_examen;
+
+public class Ejercicio4AjedrezMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ .history/EjerciciosClasesJava/src/ej_examen/Ejercicio4AjedrezMain_20241128125203.java
@@ -0,0 +1,70 @@
+package ej_examen;
+
+import ej_examen.Pieza.Color;
+import ej_examen.Pieza.TipoPieza;
+
+public class Ejercicio4AjedrezMain {
+
+	public static void main(String[] args) {
+		//Construye el tablero
+		//8 filas, 8 columnas
+		JuegoAjedrez juegoAjedrez = new JuegoAjedrez();
+
+		//Obtener movimiento de torre blanca
+		System.out.println(TipoPieza.R.getTipoMovimiento());
+		
+				//Poner reina blanca en 3,3
+				juegoAjedrez.ponerPieza((byte) 3, (byte) 3, new Pieza(TipoPieza.Q, Color.B));
+		
+				//Poner rey blanco en 5,5
+				juegoAjedrez.ponerPieza((byte) 5, (byte) 5, new Pieza(TipoPieza.K, Color.B));
+		
+				//Mostrar piezas y su posición
+				juegoAjedrez.mostrarSituacionPiezasTablero();
+			}
+		
+		}
+		
+		
+class Pieza {
+	enum TipoPieza {
+			R, //Rook
+			N, //kNight
+			B, //Bishop
+			K, //King
+			Q, //Queen
+			P; //Pawn
+	
+			//TODO
+			String getTipoMovimiento() {
+				return "";
+			} 
+	}
+
+	enum Color {
+		B, //Blanco
+		N //Negro
+	}
+
+	TipoPieza tipoPieza;
+	Color color;
+
+	public Pieza(TipoPieza tipoPieza, Color color) {
+		this.tipoPieza = tipoPieza;
+		this.color = color;
+	}
+}
+
+class JuegoAjedrez {
+
+	Pieza[][] PosicionesTablero = new Pieza[8][8];
+
+	void ponerPieza(byte fila, byte columna, Pieza pieza){
+		//TODO
+		PosicionesTablero[7][7] = new Pieza(TipoPieza.K, Color.B);
+	}
+
+    void mostrarSituacionPiezasTablero() {
+        //TODO
+    }
+}
