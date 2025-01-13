c6673cf555cadbecf4cfb7548a9de99f4f8ad8ec
Gab <ausejolamadridg@gmail.com>
2025-01-13 23:36:54 +0100

    ejentornos

---
+++ EjerciciosEntornos/src/errores/NumeroPrimo.java
@@ -0,0 +1,31 @@
+package errores;
+
+import java.util.Scanner;
+
+public class NumeroPrimo {
+	public static void main(String[] args) {
+		System.out.println("Gabriel Ausejo Lamadrid");
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Ingrese un número: ");
+		int numero = scanner.nextInt();
+		scanner.close();
+		if (esPrimo(numero)) {
+			System.out.println(numero + " sí es un número primo.");
+		} else {
+			System.out.println(numero + " no es un número primo.");
+		}
+	}
+
+	public static boolean esPrimo(int num) {
+		if (num <= 1) {
+			return false;
+		}
+		for (int i = 2; i <= num; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
