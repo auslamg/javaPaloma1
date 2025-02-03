9ac75c35792430cd3b566283eb67889d1020aa28
Gab <ausejolamadridg@gmail.com>
2025-02-03 09:17:33 +0100

    HaggleBot

--- EjerciciosClasesJava/src/juegosclase/haggle/HaggleBot.java
+++ EjerciciosClasesJava/src/juegosclase/haggle/HaggleBot.java
@@ -1,22 +1,85 @@
 package juegosclase.haggle;
 
-import java.util.ArrayList;
-import java.util.List;
+import gab.utils.console.ConsoleReader;
+import java.util.Random;
 
 @SuppressWarnings("unused")
 public class HaggleBot {
+	
 	public static void main(String[] args) {
-		//Integer name = ConsoleReader.askT("What is your name?", Integer.class);
+		
+		boolean isTrading = true;
 
-		//System.out.println("Hola "	+ name + ", bienvenido a HaggleBot");
+		float baseValue = 100;
+		float maxValue = baseValue * 1.5f; //Initial value
+		float minValue = baseValue *1.1f; //Minimum sell value to take
+
+		float currentValue = maxValue;
+
+		ActorActions myAction = null;
+
+		while (isTrading) { 
+			System.out.println("Current value is: " + currentValue);
+
+			//Mayor que el minimo
+			if (currentValue > minValue) {
+				ConsoleReader c = new ConsoleReader();
+				myAction = c.askEnumMemberByListIndex("What action do you want to make?", ActorActions.class); 
+			}
+			//Ultima oferta
+			else {
+				System.out.println("Price went below minPrice, finishing trading");
+				isTrading = false; //Terminar
+
+				ActorActions2 myLastAction = null;
+				myLastAction = new ConsoleReader().askEnumMemberByListIndex("What action do you want to make?", ActorActions2.class); 
+
+				//Ultima acción
+				switch (myLastAction) {
+					case BUY:
+						isTrading = false;
+						break;
+					default:
+						isTrading = false;
+				}
+
+				return;
+			}
+
+			//Acción
+			switch (myAction) {
+				case BUY -> {
+					isTrading = false; //Terminar
+				}
+				case Lower_price -> {
+					Random r = new Random();
+					float randomNum = r.nextFloat(minValue,currentValue);
+					float roundedNumber = 5*Math.round(randomNum/5);
+					currentValue = roundedNumber;
+				}
+				default -> throw new AssertionError();
+			}
+
+			System.out.println("Current value is now: " + currentValue);
+			if (!isTrading) {
+				System.out.println("Trading has finished");
+			}
+		}
 	}
 }
 
 enum ActorActions {
-	SELL,
-	BUY
+	BUY,
+	Lower_price
 }
 
+enum ActorActions2 {
+	BUY,
+	LEAVE
+}
+
+
+/* 
 class Offer {
 	Actor actor;
 	int priceOffer;
@@ -58,4 +121,4 @@ class Article {
 		this.name = name;
 		this.baseValue = baseValue;
 	}
-}
+} */
