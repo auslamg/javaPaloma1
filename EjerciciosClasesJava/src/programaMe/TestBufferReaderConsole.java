cbdde520d3af9544ca944c83cff333fabb2f5760
Gab <ausejolamadridg@gmail.com>
2025-02-18 14:23:26 +0100

    Create TestBufferReaderConsole.java

---
+++ EjerciciosClasesJava/src/programaMe/TestBufferReaderConsole.java
@@ -0,0 +1,22 @@
+package programaMe;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestBufferReaderConsole {
+    
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+
+        }
+    }    
+}
