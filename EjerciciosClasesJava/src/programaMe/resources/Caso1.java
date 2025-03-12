78d0616cce258ec867d89754d46392678608636b
Gab <ausejolamadridg@gmail.com>
2025-03-12 16:12:45 +0100

    Added templates

---
+++ EjerciciosClasesJava/src/programaMe/resources/Caso1.java
@@ -0,0 +1,16 @@
+package programaMe.resources;
+
+import java.util.Scanner;
+
+public class Caso1 {
+
+	static Scanner in = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int casos = in.nextInt();
+
+		for (int i = 0; i < casos; i++) {
+			//Programa
+		}
+	}
+}
